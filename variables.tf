20f37d85b571113fbf3425426449334b19493030
siddharthchouradiya <60422240+siddharthchouradiya@users.noreply.github.com>
2021-03-03 10:37:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,42 +1,44 @@
 variable "access_key" {
-  default = "Enter Your Access Key"
+  default = ""          #Enter Your Access Key
 }
 
 variable "secret_key" {
-  default = "Enter Your Secret Key"
+  default = ""          #Enter Your Secret Key
 }
 
 
 variable "region" {
-  default = "Enter Your AWS Region"
+  default = ""          #Enter Your AWS Region
 }
 
 variable "ami" {
-  default = "Enter Your AWS AMI"
+  default = ""          #Enter Your AWS AMI
 }
 
 variable "key_name" {
-  default = "Enter Your Key Pair Name without extention"
+  default = ""          #Enter Your Key Pair Name without extention
+}
+
+variable "pem_file" {
+  default = ""          #Enter complete pem file
 }
 
 variable "magento" {
-  default = "Enter the path of magento tar file"
+  default = ""          #Enter the path of magento tar file
 }
 
 variable "mysql_user" {
-  default = "Enter Your Mysql Username"
+  default = ""          #Enter Your Mysql Username
 }
 
 variable "mysql_db" {
-  default = "Enter Your Mysql Database Name"
+  default = ""          #Enter Your Mysql Database Name
 }
 
 variable "mysql_pass" {
-  default = "Enter Your Mysql Password"
+  default = ""          #Enter Your Mysql Password
 }
 
 variable "rds_identifier" {
-  default = "Enter RDS-Identifier Name"
+  default = ""          #Enter RDS-Identifier Name
 }
-
-
