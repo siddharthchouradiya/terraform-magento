530eff336a703c1c77a33d69dfe0c0c660783a8c
siddharthchouradiya <60422240+siddharthchouradiya@users.noreply.github.com>
2021-03-03 09:54:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "first" {
   ami           = var.ami
   instance_type = "t2.micro"
   key_name      = var.key_name
-  security_groups = ["${aws_security_group.first.name}"]
+  security_groups = [aws_security_group.first.name]
   tags = {
     Name = "webserver"
   }
@@ -24,11 +24,6 @@ resource "aws_instance" "first" {
     #restrict permission: chmod 400 <your_private_key>.pem
     private_key = file("/path/of/pem/file")
     host        = self.public_ip
-  }
-
-  provisioner "file" {
-    source      = var.magento
-    destination = "/home/ubuntu/magento.tar.gz" 
   }
   
   provisioner "file" {
